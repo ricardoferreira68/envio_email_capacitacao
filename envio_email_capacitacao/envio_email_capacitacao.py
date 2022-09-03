6b462a3c48cbef76cb87559d7e16bcb4925e35c1
ricardoferreira68 <jrsf68@gmail.com>
2022-09-03 09:46:24 -0300

    Iniciando o projeto de envio de mensagem.

---
+++ envio_email_capacitacao/envio_email_capacitacao.py
@@ -0,0 +1,7 @@
+"""Envia mensagem por e-mail aos participantes da capacitação a patir de um arquivo csv.
+
+    Dado que: os dados dos participantes na capacitação são inseridos na planilha de controle 
+        e a aba "csv_mensagem" é formatada com os dados da mensagem com email|assunto|corpo|
+    Quando: for gerado o arquivo "mensagem_capacitacao.csv" é gerado com a opção "Salvar Como"
+    Então: enviar e-mail para cada participante encontrado no arquivo "mensagem_capacitacao.csv"
+"""
