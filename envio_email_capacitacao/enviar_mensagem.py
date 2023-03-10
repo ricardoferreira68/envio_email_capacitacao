add72f0073515b50b3423da75149220c86c16cc4
ricardoferreira68 <jrsf68@gmail.com>
2023-03-10 18:07:34 +0000

    pdf_files folder

--- envio_email_capacitacao/enviar_mensagem.py
+++ envio_email_capacitacao/enviar_mensagem.py
@@ -1,17 +1,15 @@
 """Envia mensagem por e-mail a partir do arquivo csv.
 """
 
-fr''
-import pandas as pd  # Usado para ler arquivo .csv
-import numpy
+
 import smtplib
 from email import encoders
 from email.message import Message
 from email.mime.text import MIMEText 
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
-import os
-import ssl
+from pandas import read_csv
+from pandas.core.frame import DataFrame
 
 
 def enviar_emails(arquvio_csv: str) -> None:
@@ -38,7 +36,7 @@ def enviar_emails(arquvio_csv: str) -> None:
             print(f"{contador}, ")
 
 
-def le_arquivo_com_dados_das_mensagens(arquivo: str) -> pd.core.frame.DataFrame:
+def le_arquivo_com_dados_das_mensagens(arquivo: str) -> DataFrame:
     """ Ler o arquivo com as mensagens a serem enviadas
         Linha de mensagem finalizada com | (para aceitar enter no meio da mensagem).
 
@@ -46,10 +44,10 @@ def le_arquivo_com_dados_das_mensagens(arquivo: str) -> pd.core.frame.DataFrame:
         arquivo (str): nome do arquivo .csv gravado em disco.
 
     Returns:
-        pd.core.frame.DataFrame: dataframe pandas contendo as mensagens lidas do arquivo.
+        DataFrame: dataframe pandas contendo as mensagens lidas do arquivo.
     """
 
-    dataframe_com_dados_das_mensagens = pd.read_csv(
+    dataframe_com_dados_das_mensagens = read_csv(
         arquivo, sep='|', header=[0])
 
     return dataframe_com_dados_das_mensagens
@@ -78,7 +76,8 @@ def enviar_email(email: str, assunto: str, corpo: str, anexos: list) -> None:
     msg = MIMEMultipart() 
     msg['From'] = "joseferreira@lampp-it.com.br"
     msg['To'] = email
-    msg['Bcc'] = "joseferreira@lampp-it.com.br"
+    msg['Cc'] = ""  # "dp@lampp-it.com.br"
+    msg['Cc2'] = "joseferreira@lampp-it.com.br"  # Cópia oculta.
     msg['Subject'] = assunto
     msg.add_header("Content-Type", "Content-Transfer-Encoding")
     
@@ -101,4 +100,7 @@ def enviar_email(email: str, assunto: str, corpo: str, anexos: list) -> None:
   
     msg.attach(MIMEText(corpo, 'html')) 
     server.login(msg['From'], "lA2021,.aL&uk")  # os.environ["PASSWORD"])
-    server.sendmail(msg['From'], [msg['To'], msg['Bcc']], msg.as_string().encode("utf-8"))
+    to_receivers = [msg['To']]
+    if msg['Cc'] != "" and msg['Cc'] is not None: to_receivers.append(msg['Cc'])
+    if msg['Cc2'] != "" and msg['Cc2'] is not None: to_receivers.append(msg['Cc2'])
+    server.sendmail(msg['From'], to_receivers, msg.as_string().encode("utf-8"))
